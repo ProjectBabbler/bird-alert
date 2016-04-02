f8f5beab04f5b84bc8c5e4881ad659250b691341
Greg Schechter <diabolomaster@gmail.com>
2016-04-01 19:36:41 -0700

    Initical commit

---
+++ index.ios.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+class birdalert extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./images/logo.png')} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('birdalert', () => birdalert);
