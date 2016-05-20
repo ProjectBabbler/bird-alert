633f9d8b8aadcc96deb6f810136da9335d5bf1b0
Greg Schechter <diabolomaster@gmail.com>
2016-05-20 08:42:53 -0700

    Process needs for me in california

--- jobs/BatchCreator.js
+++ jobs/BatchCreator.js
@@ -5,28 +5,44 @@ var queue = kue.createQueue({
   redis: process.env.REDIS_URL,
 });
 
-locations.getAll().then(list => {
-    var ps = Object.keys(list).map(code => {
-        return new Promise((resolve, reject) => {
-            let job = queue.create('notable', {
-                title: `fetch notable for ${code}`,
-                code: code,
-            })
-            .removeOnComplete(true)
-            .save(err => {
-                if (!err) {
-                    console.log(job.id, `fetch notable for ${code}`);
-                }
-                resolve();
+let username = 'buskergreg';
+let code = 'US-CA';
+new Promise((resolve, reject) => {
+    let job = queue.create('need', {
+        title: `fetch need for ${username} ${code}`,
+        code: code,
+        username: username,
+    })
+    .removeOnComplete(true)
+    .save(err => {
+        if (!err) {
+            console.log(job.id, `fetch need for ${username} ${code}`);
+        }
+        resolve();
+    });
+}).then(() => {
+    locations.getAll().then(list => {
+        var ps = Object.keys(list).map(code => {
+            return new Promise((resolve, reject) => {
+                let job = queue.create('notable', {
+                    title: `fetch notable for ${code}`,
+                    code: code,
+                })
+                .removeOnComplete(true)
+                .save(err => {
+                    if (!err) {
+                        console.log(job.id, `fetch notable for ${code}`);
+                    }
+                    resolve();
+                });
             });
         });
-    });
 
-    Promise.all(ps).then(() => {
-        process.exit(0);
-    }).catch((e) => {
-        console.log(e);
-        process.exit(1);
-    })
-});
+        return Promise.all(ps);
+}).then(() => {
+    process.exit(0);
+}).catch((e) => {
+    console.log(e);
+    process.exit(1);
+})
 
--- jobs/ProcessNotable.js
+++ jobs/ProcessNotable.js
@@ -1,18 +1,32 @@
 var kue = require('kue');
 var ebirdjs = require('ebird-js');
+var ebird = require('ebird');
 var firebase = require('./firebase');
 var queue = kue.createQueue({
   redis: process.env.REDIS_URL,
 });
 
-queue.process('notable', 3, (job, done) => {
+queue.process('notable', 1, (job, done) => {
     console.log(`Processing for ${job.data.code}`);
     ebirdjs.data.notable.region.recent({
         r: job.data.code,
         back: 2,
     }).then(results => {
         firebase.database().ref('ebird/notable').child(job.data.code).set(results).then(() => {
-            console.log(`Saving ${job.data.code}`);
+            console.log(`Saved ${job.data.code}`);
+        }).then(done);
+    });
+});
+
+queue.process('need', 1, (job, done) => {
+    console.log(`Processing for ${job.data.username} ${job.data.code}`);
+    var instance = new ebird();
+    instance.auth(job.data.username, process.env.TEMP_PASSWORD).then(() => {
+        return instance.need(job.data.code);
+    }).then(results => {
+        console.log(results);
+        firebase.database().ref('ebird/need').child(job.data.username).child(job.data.code).set(results).then(() => {
+            console.log(`Saved ${job.data.username} ${job.data.code}`);
         }).then(done);
     });
 });
