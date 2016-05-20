8052a4fe218eaf024dbf1504b6f59eb2a9c79637
Greg Schechter <diabolomaster@gmail.com>
2016-05-19 23:53:57 -0700

    Let BatchCreator Finish

--- jobs/BatchCreator.js
+++ jobs/BatchCreator.js
@@ -6,17 +6,27 @@ var queue = kue.createQueue({
 });
 
 locations.getAll().then(list => {
-    for (var code in list) {
-        let job = queue.create('notable', {
-            title: `fetch notable for ${code}`,
-            code: code,
-        })
-        .removeOnComplete(true)
-        .save(err => {
-            if (!err) {
-                console.log(job.id, `fetch notable for ${code}`);
-            }
+    var ps = Object.keys(list).map(code => {
+        return new Promise((resolve, reject) => {
+            let job = queue.create('notable', {
+                title: `fetch notable for ${code}`,
+                code: code,
+            })
+            .removeOnComplete(true)
+            .save(err => {
+                if (!err) {
+                    console.log(job.id, `fetch notable for ${code}`);
+                }
+                resolve();
+            });
         });
-    }
+    });
+
+    Promise.all(ps).then(() => {
+        process.exit(0);
+    }).catch((e) => {
+        console.log(e);
+        process.exit(1);
+    })
 });
 
